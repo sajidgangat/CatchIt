665ebce930503b7ffa6f58e775be2451a1dfe341
Paul <paul@Pauls-MacBook-Air.local>
2017-06-23 15:00:52 -0400

    changes today at 2 pm where the notes table list view is crashing

---
+++ catchit/EditProspectViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProspectTableviewControllerTableViewController.swift
+//  catchit
+//
+//  Created by Paul on 2017-06-21.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+import UIKit
+
+class ProspectTableviewControllerTableViewController: UIViewController {
+
+     @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var TextboxCompany: UITextField!
+    
+    @IBOutlet weak var TextboxCity: UITextField!
+    @IBOutlet weak var TextboxAddress: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(scrollView)
+       // var stringpassed = ""
+         TextboxCity.text = "Toronto"
+         TextboxCompany.text = "Bermin And Co"
+         TextboxAddress.text = "Lee Center Dr"
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        scrollView.contentSize = CGSize(width: 375, height: 1200)
+    }
+    
+    
+    
+    }
+    
+
