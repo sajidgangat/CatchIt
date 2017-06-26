477d33817775d951cbb17fe23e9a68c8479383e7
Paul <paul@Pauls-MacBook-Air.local>
2017-06-26 18:28:25 -0400

    latest

--- catchit/AddNoteViewController.swift
+++ catchit/AddNoteViewController.swift
@@ -19,35 +19,47 @@ class AddNoteViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func AddNote(_ sender: Any) {
-        
+    
+    func createQuestions(handleComplete:(()->())){
         var request = URLRequest(url: URL(string: "https://breezemaxlabs.com/RestAPI/v1/createNote")!)
         request.httpMethod = "POST"
         let postString = "email=nik@breeze.com&subject=\(subjectTextBox.text!)&note=\(NoteTextBox.text!)"
         request.httpBody = postString.data(using: .utf8)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else
-            {                                                 // check for fundamental networking error
-                print("error=\(String(describing: error))")
-                return
+                   let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data, error == nil else
+                {                                                 // check for fundamental networking error
+                    print("error=\(String(describing: error))")
+                    return
+                }
+                
+                let responseString = String(data: data, encoding: .utf8)
+                self.status = responseString
+                print("responseString = \(String(describing: responseString))")
+                
             }
-            
-            let responseString = String(data: data, encoding: .utf8)
-            self.status = responseString
-            print("responseString = \(String(describing: responseString))")
-            
-            
-        }
-        task.resume()
-        
-        popToRoot()
+            task.resume()
+
+
+        handleComplete() // call it when finished s.t what you want
     }
-    func popToRoot() {
-        
+    
+    func newQuestion(){
         let alert = UIAlertController(title: "Alert", message: "\(String(describing: status)))", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
+    
+    
+    
+    @IBAction func AddNote(_ sender: Any) {
+        
+        self.createQuestions { () -> () in
+            self.newQuestion()
+        }
+        
+        
+    }
+    
 }
 
 
